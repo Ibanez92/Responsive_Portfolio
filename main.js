e7ebacf8c1f46e0d0c83326759082bbd25966160
Javier Ibanez <expertprep2015@gmail.com>
2021-03-17 16:13:40 -0400

    added javascript and emailjs to send the form with name, email, and message to my email

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 
+
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', ()=>{
@@ -24,3 +25,4 @@ menu_item.forEach((item) =>{
         mobile_menu.classList.toggle('active');
     });
 });
+
